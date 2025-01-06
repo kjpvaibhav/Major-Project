6b1f4a9f7d57c88427a7fdbdfcaa0d8bb2558a8d
Kamarajuggada Jyothi Phani Vaibhav <84036069+kjpvaibhav@users.noreply.github.com>
2025-01-06 17:45:53 +0530

    Uploaded via K.J.P.Vaibhav's PC

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,7 @@
-api_speech = "15b649ca04a1411a80ed07f7f07fffb3"
-grog_api = "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"
-API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
-HEADERS = {"Authorization": "Bearer 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"}
-MONGO_URI = "mongodb+srv://Admin:Seemanth11@cluster0.5zclou7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+api_speech = "YOUR_API_HERE"
+grog_api = "YOUR_API_HERE"
+API_URL = "YOUR_API_URL_HERE"
+HEADERS = {"Authorization": "Bearer hf_FM"}
+MONGO_URI = "YOUR_API_URL_HERE"
 DB_NAME = "question_answers"
 COLLECTION_NAME = "questions"
